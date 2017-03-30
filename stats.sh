d3e2f21df7d52beaeab35774be090f00c5304f53
grcgrc <grctestemail@gmail.com>
2017-03-30 12:50:35 +0100

    stats.sh fix

--- stats.sh
+++ stats.sh
@@ -2,17 +2,17 @@
 function node-stats {
    #echo "Gridcoin client running. Creating files!"
 
-    grc getinfo > /usr/share/netdata/web/getinfo.json
+	grc getinfo > /home/gridcoin/.GridcoinResearch/getinfo.json
     grc getdifficulty > /home/gridcoin/.GridcoinResearch/difficulty.json
 
     #echo "Moving files"
     # \cp getinfo.json /usr/share/netdata/web/
 
     #echo "Changing file ownership"
-    sudo chown netdata:netdata /usr/share/netdata/web/getinfo.json
+    # sudo chown netdata:netdata /usr/share/netdata/web/getinfo.json
 
     #echo "Deleting files"
-    rm -rf getinfo.json
+    # rm -rf getinfo.json
 }
 
 # Check if gridcoin is running, call node stats script
