7209c28aaea7cc68c8e0ecfa6ecc65fecc99c4ad
grcgrc <grctestemail@gmail.com>
2017-04-02 17:42:47 +0100

    getpeerinfo.hs works now!
    Now we just need to update stats.sh to call this script, then add the new charts in the netdata chart plugin!

--- getpeerinfo.hs
+++ getpeerinfo.hs
@@ -16,10 +16,8 @@ import Turtle
 
 -- | Declaring fancy types
 -- Not neccessary, but makes interpretation of the script easier.
-type InBound = Bool
-type StartingHeight = Int
-type Trust = Int
-type SubVer = String
+type InBound_or_SubVer = String
+type StartingHeight_or_Trust = Int
 
 -- | Global variables
 -- Change these filepath values at your discretion
@@ -27,7 +25,6 @@ versions_filepath = "peerinfo_versions.txt"
 trust_filepath = "peerinfo_trust.txt"
 height_filepath = "peerinfo_height.txt"
 bound_filepath = "peerinfo_bound.txt"
-empty_file = ""
 
 -- | Configuring Aeson to handle the getpeerinfo.json fields
 data GetPeerInfo = GetPeerInfo { subver :: String
@@ -36,7 +33,12 @@ data GetPeerInfo = GetPeerInfo { subver :: String
                                  , nTrust :: Int
                                  } deriving (Show, Generic, Eq)
 
-data ListInputTypes = InBound | StartingHeight | Trust | SubVer
+-- | Currently having issues using the custom data type.
+-- Step 1 is to write the code messily.
+-- Step 2 is to consolodate code.
+--data OrderedListInputTypes = InBoundD [InBound] | SubVerD SubVer deriving (Show, Generic, Eq)
+
+-- data UnorderedTypes = StartingHeightD [StartingHeight] | TrustD [Trust] deriving (Show, Generic, Eq)
 
 instance ToJSON GetPeerInfo where
     toEncoding = genericToEncoding defaultOptions
@@ -67,88 +69,99 @@ jsonFile :: Prelude.FilePath
 jsonFile = "getpeerinfo.json"
 
 -- | Full JSON data in, list of subver out
-jsonToSubVerList :: [GetPeerInfo] -> [SubVer]
+jsonToSubVerList :: [GetPeerInfo] -> [InBound_or_SubVer]
 jsonToSubVerList [] = []
 jsonToSubVerList (x:xs) = do
     [subver x] ++ jsonToSubVerList xs
 
 -- | Full JSON data in, list of nTrust out
-jsonToTrustList :: [GetPeerInfo] -> [Trust]
+jsonToTrustList :: [GetPeerInfo] -> [StartingHeight_or_Trust]
 jsonToTrustList [] = []
 jsonToTrustList (x:xs) = do
     [nTrust x] ++ jsonToTrustList xs
 
 -- | Full JSON data in, list of startingHeights out
-jsonToStartingHeightList :: [GetPeerInfo] -> [StartingHeight]
+jsonToStartingHeightList :: [GetPeerInfo] -> [StartingHeight_or_Trust]
 jsonToStartingHeightList [] = []
 jsonToStartingHeightList (x:xs) = do
     [startingheight x] ++ jsonToStartingHeightList xs
 
+-- | Converting boolean to string for output to text file
+-- source: https://stackoverflow.com/questions/37019716/how-do-we-convert-boolean-to-string-in-haskell
+boolToString :: Bool -> String
+boolToString True = "TRUE"
+boolToString False = "FALSE"
+
 -- | Full JSON data in, list of inbound values
 -- Establish the quantity|percent of connections which full node is seeding.
 -- Area chart! (x% false, y% true; x+y=100%) 
-jsonToBoundList :: [GetPeerInfo] -> [InBound]
+jsonToBoundList :: [GetPeerInfo] -> [InBound_or_SubVer]
 jsonToBoundList [] = []
 jsonToBoundList (x:xs) = do
-    [inbound x] ++ jsonToBoundList xs
+    -- | Convert boolean value to string, as bools cannot be written to disk.
+    -- Kinda ugly, but deal w/ it.
+    let currentBoolVal = boolToString (inbound x)
+    -- | Write current bool string to list
+    -- Recursively call jsonToBoundList, passing the rest of the list in as input.
+    [currentBoolVal] ++ jsonToBoundList xs
 
 -- | Counting occurrence of item in a list
 -- source: https://stackoverflow.com/questions/19554984/haskell-count-occurrences-function
 countIt :: Eq a => a -> [a] -> Int
 countIt x = length . filter (x==)
 
-countedList :: [ListInputTypes] -> [ListInputTypes] ->  [(ListInputTypes, Int)]
+countedList :: [InBound_or_SubVer] -> [InBound_or_SubVer] ->  [(InBound_or_SubVer, Int)]
 countedList [] referenceList = []
 countedList (x:xs) referenceList = do
     let countedVal = (countIt x referenceList)
+    -- | Input current 
     [(x, countedVal)] ++ countedList xs referenceList
 
 -- | Writing the contents of each element of the list to file
 -- Input the list of touples output by countedList
-outputCountedList :: [(ListInputTypes, Int)] -> Prelude.FilePath -> IO ()
+outputCountedList :: [(InBound_or_SubVer, Int)] -> Prelude.FilePath -> IO ExitCode
 outputCountedList (x:xs) txtFilePath = do
-    let contents = T.pack((fst x) ++ " " ++ (show (snd x)))
-
+    --let contents = T.pack((fst x) ++ " " ++ (show (snd x)))
+    let contents = ((fst x)) ++ " " ++ (show (snd x))
+    --print contents
     -- | How to catch empty xs when the output is IO?
     -- Perhaps worth trying "outputCountedList [] txtFilePath = []" for less code reuse
     if (xs == [])
         then do
-            (appendFile txtFilePath contents)
+            shell (T.pack ("echo '" ++ contents ++ "' >> " ++ txtFilePath)) Turtle.empty
+            --(appendFile txtFilePath contents)
         else do
-            (appendFile txtFilePath contents)
+            shell (T.pack ("echo '" ++ contents ++ "' >> " ++ txtFilePath)) Turtle.empty
+            --(appendFile txtFilePath contents) -- Does appendFile cut off the next line from running? Might need to switch for shell!
             outputCountedList xs txtFilePath
 
-outputUnorderedList :: [String] -> Prelude.FilePath -> IO ()
+outputUnorderedList :: [StartingHeight_or_Trust] -> Prelude.FilePath -> IO ExitCode
 outputUnorderedList (x:xs) txtFilePath = do
+    --print x
     -- | How to catch empty xs when the output is IO?
     -- Perhaps worth trying "outputCountedList [] txtFilePath = []" for less code reuse
     if (xs == [])
         then do
-            (appendFile txtFilePath x)
+            shell (T.pack ("echo '" ++ (show x) ++ "' >> " ++ txtFilePath)) Turtle.empty
         else do
-            (appendFile txtFilePath x)
-            outputUnorderedList xs txtFilePath
+            shell (T.pack ("echo '" ++ (show x) ++ "' >> " ++ txtFilePath)) Turtle.empty
+            outputUnorderedList xs txtFilePath -- Does appendFile cut off the next line from running? Might need to switch for shell!
 
 -- | Main function that is called from prelude!
 main :: IO ()
 main = do
 
-    shell "echo '' > peerinfo_versions.txt" Turtle.empty
-    shell "echo '' > peerinfo_trust.txt" Turtle.empty
-    shell "echo '' > peerinfo_height.txt" Turtle.empty
-    shell "echo '' > peerinfo_bound.txt" Turtle.empty
-
-
-    -- | Fancy way of clearing the file
-    -- Doesn't work, this is IO & ends the output (shell line etc isn't called).
-    -- benchmark shell vs writefile? perhaps just delete
-    --writeFile versions_filepath empty_file --sVList
-    --writeFile trust_filepath empty_file --nTList
-    --writeFile height_filepath empty_file --shList
-    --writeFile bound_filepath empty_file --ibList
+    -- | Empty the contents of the existing files!
+    -- -n to prevent adding an empty line to the file
+    shell "echo -n '' > peerinfo_versions.txt" Turtle.empty
+    shell "echo -n '' > peerinfo_trust.txt" Turtle.empty
+    shell "echo -n '' > peerinfo_height.txt" Turtle.empty
+    shell "echo -n '' > peerinfo_bound.txt" Turtle.empty
 
     -- | gridcoinresearchd until the script is proven working in VM
-    shell ("gridcoinresearchd getpeerinfo > echo > getpeerinfo.json") Turtle.empty
+    -- You will need to change this depending on your gridcoin setup!
+    -- shell ("gridcoinresearchd getpeerinfo > echo > getpeerinfo.json") Turtle.empty
+    shell ("grc getpeerinfo > echo > getpeerinfo.json") Turtle.empty
 
     -- | Finally works! Reads getpeerinfo.json from disk into memory!
     -- From this point onwards, we can reuse getpeerinfoJSON.
@@ -156,26 +169,16 @@ main = do
 
     -- | Converting JSON to seperate lists
     let sVList = (jsonToSubVerList (fromJust gerpeerinfoJSON))
-    --let nTList = (jsonToTrustList (fromJust gerpeerinfoJSON))
-    --let shList = (jsonToStartingHeightList (fromJust gerpeerinfoJSON))
-    --let ibList = (jsonToBoundList (fromJust gerpeerinfoJSON))
+    let nTList = (jsonToTrustList (fromJust gerpeerinfoJSON))
+    let shList = (jsonToStartingHeightList (fromJust gerpeerinfoJSON))
+    let ibList = (jsonToBoundList (fromJust gerpeerinfoJSON))
 
     -- | Writing data to text files
     -- Counted lists (frequency of occurrence)
-    outputCountedList (countedList sVList) versions_filepath 
-    --outputCountedList nTList trust_filepath --Trust could be totaled or averaged, perhaps better than 200 lines (less resource hungry)
-    --outputCountedList ibList bound_filepath 
+    outputCountedList (countedList (nub sVList) sVList) versions_filepath
+    outputCountedList (countedList (nub ibList) ibList) bound_filepath
+    -- Unorered output to text files
+    outputUnorderedList shList height_filepath
+    outputUnorderedList nTList trust_filepath
 
-    -- | Writing data to text file
-    -- Unsorted data
-    --outputUnorderedList shList height_filepath
-
-    print "End!"
-
-{-|
--- Example getpeerinfo in list format
-["/Nakamoto:3.5.8.7(317.1999)/","/Nakamoto:3.5.8.7(317.1999)/","/Nakamoto:3.5.8.7(317.1999)/","/Nakamoto:3.5.8.7(317.1999)/","/Nakamoto:3.5.8.7(317.1999)/","/Nakamoto:3.5.8.7(0)/","/Nakamoto:3.5.8.7(0)/","/Nakamoto:3.5.8.7(0)/"]
-[9,2,1,2,1,1,0,2]
-[858608,858608,858608,858608,858608,858609,858610,858610]
-[False,False,False,False,False,False,False,False]
--}
+    --print "End!"
