83a10927d2a09d56095404bed8059f60af28871e
grcgrc <grctestemail@gmail.com>
2017-04-03 01:43:04 +0100

    name change

--- getpeerinfo/getpeerinfo.chart.sh
+++ getpeerinfo/getpeerinfo.chart.sh
@@ -6,35 +6,35 @@
 # GPL v3+
 #
 
-gridcoin_getpeerinfo_update_every=10
+getpeerinfo_update_every=10
 load_priority=2
 
 # this is an example charts.d collector
 # it is disabled by default.
 # there is no point to enable it, since netdata already
 # collects this information using its internal plugins.
-gridcoin_getpeerinfo_enabled=1
+getpeerinfo_enabled=1
 
-gridcoin_getpeerinfo_check() {
+getpeerinfo_check() {
 	# this should return:
 	#  - 0 to enable the chart
 	#  - 1 to disable the chart
 
-	if [ ${gridcoin_getpeerinfo_update_every} -lt 10 ]
+	if [ ${getpeerinfo_update_every} -lt 10 ]
 		then
 		# there is no meaning for shorter than 5 seconds
 		# the kernel changes this value every 5 seconds
-		gridcoin_getpeerinfo_update_every=10
+		getpeerinfo_update_every=10
 	fi
 
-	[ ${gridcoin_getpeerinfo_enabled} -eq 0 ] && return 1
+	[ ${getpeerinfo_enabled} -eq 0 ] && return 1
 	return 0
 }
 
-gridcoin_getpeerinfo_create() {
+getpeerinfo_create() {
         # create a chart with 3 dimensions
 cat <<EOF
-CHART GRC.PeerVersions '' "Gridcoin peer versions" "client version" Versions GRC.PeerVersions line $((load_priority + 1)) $gridcoin_getpeerinfo_update_every
+CHART GRC.PeerVersions '' "Gridcoin peer versions" "client version" Versions GRC.PeerVersions line $((load_priority + 1)) $getpeerinfo_update_every
 while read data
 do
     currentLine="$data"
@@ -46,7 +46,7 @@ EOF
         return 0
 }
 
-gridcoin_getpeerinfo_update() {
+getpeerinfo_update() {
         # write the result of the work.
         cat <<VALUESEOF
 BEGIN GRC.PeerVersions
