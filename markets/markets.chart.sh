2a95f8a1f90bf54be8b319037f6e3c9610ff0692
grcgrc <grctestemail@gmail.com>
2017-04-04 20:31:52 +0000

    fix

--- markets/markets.chart.sh
+++ markets/markets.chart.sh
@@ -34,15 +34,15 @@ markets_check() {
 markets_create() {
         # create a chart with 3 dimensions
 cat <<EOF
-CHART market.rank '' "Gridcoin CMC Rank" "Rank" GRC_Rank market.rank stacked $((load_priority + 1)) $markets_update_every
+CHART market.rank '' "Gridcoin CMC Rank" "Rank" GRC_Rank market.rank line $((load_priority + 1)) $markets_update_every
 DIMENSION rank 'Rank' absolute 1 1
-CHART market.price '' "Gridcoin price" "Price" GRC_Price market.price stacked $((load_priority + 1)) $markets_update_every
+CHART market.price '' "Gridcoin price" "Price" GRC_Price market.price line $((load_priority + 1)) $markets_update_every
 DIMENSION usd 'USD' absolute 1 1
 DIMENSION btc 'BTC' absolute 1 1
-CHART market.capandliquidity '' "Gridcoin marketcap & liquidity" "marketcap & liquidity" MarketCap_and_Liquidity market.capandliquidity stacked $((load_priority + 1)) $markets_update_every
+CHART market.capandliquidity '' "Gridcoin marketcap & liquidity" "marketcap & liquidity" MarketCap_and_Liquidity market.capandliquidity line $((load_priority + 1)) $markets_update_every
 DIMENSION volume 'Volume' absolute 1 1
 DIMENSION cap 'Market Cap' absolute 1 1
-CHART market.percent_change '' "Gridcoin percent change" "Gridcoin percent change" GRC_Percent_Changes market.percent_change stacked $((load_priority + 1)) $markets_update_every
+CHART market.percent_change '' "Gridcoin percent change" "Gridcoin percent change" GRC_Percent_Changes market.percent_change line $((load_priority + 1)) $markets_update_every
 DIMENSION onehour '1Hr' absolute 1 1
 DIMENSION twentyfour '24Hr' absolute 1 1
 DIMENSION sevendays '7days' absolute 1 1
@@ -94,6 +94,7 @@ SET cap = $capVal
 BEGIN market.percent_change
 SET onehour = $onehourVal
 SET twentyfour = $twentyfourVal
+SET sevendays = $sevendaysVal
 VALUESEOF
 
         return 0
