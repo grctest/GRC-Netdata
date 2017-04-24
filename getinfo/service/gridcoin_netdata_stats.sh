88c3e31fb807369bfd45a2057ba262d33a542ca8
Foggyx420 <eastendmumbles@gmail.com>
2017-04-24 11:45:24 -0700

    Update gridcoin_netdata_stats.sh
    removed difficulty

--- getinfo/service/gridcoin_netdata_stats.sh
+++ getinfo/service/gridcoin_netdata_stats.sh
@@ -6,7 +6,6 @@ then
     GRCPATH='/home/gridcoin/.GridcoinResearch'
     "$GRCAPP" getinfo > "$GRCPATH"/getinfo.tmp && mv -f "$GRCPATH"/getinfo.tmp "$GRCPATH"/getinfo.json
     "$GRCAPP" getstakinginfo > "$GRCPATH"/getstakinginfo.tmp && mv -f "$GRCPATH"/getstakinginfo.tmp "$GRCPATH"/getstakinginfo.json
-    "$GRCAPP" getdifficulty > "$GRCPATH"/difficulty.tmp && mv -f "$GRCPATH"/difficulty.tmp "$GRCPATH"/difficulty.json
     else
     exit 1
 fi
