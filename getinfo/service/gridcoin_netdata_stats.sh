dc0850f53e20b6ab15ce2bdb93e5b7d3d593630d
M. Raymond Vaughn <m.raymond.vaughn@gmail.com>
2017-07-04 10:27:42 -0400

    additional RPC metric sources

--- getinfo/service/gridcoin_netdata_stats.sh
+++ getinfo/service/gridcoin_netdata_stats.sh
@@ -6,6 +6,9 @@ then
     GRCPATH="$(getent passwd gridcoin | cut -d: -f6)/.GridcoinResearch"
     "$GRCAPP" getinfo > "$GRCPATH"/getinfo.tmp && mv -f "$GRCPATH"/getinfo.tmp "$GRCPATH"/getinfo.json
     "$GRCAPP" getstakinginfo > "$GRCPATH"/getstakinginfo.tmp && mv -f "$GRCPATH"/getstakinginfo.tmp "$GRCPATH"/getstakinginfo.json
+    "$GRCAPP" getmininginfo > "$GRCPATH"/getmininginfo.tmp && mv -f "$GRCPATH"/getmininginfo.tmp "$GRCPATH"/getmininginfo.json
+    "$GRCAPP" list mymagnitude > "$GRCPATH"/listmymagnitude.tmp && mv -f "$GRCPATH"/listmymagnitude.tmp "$GRCPATH"/listmymagnitude.json
+    "$GRCAPP" execute superblockage > "$GRCPATH"/executesuperblockage.tmp && mv -f "$GRCPATH"/executesuperblockage.tmp "$GRCPATH"/executesuperblockage.json
     else
     exit 1
 fi
